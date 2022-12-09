30fb512cab2fcfef0deb54f9e632e57f8727b101
Michael_SVAO <vierumihail7@gmail.com>
2022-12-09 11:06:41 +0300

    Задача №2

---
+++ lesson2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Количество цифр в числе = {GetCount(number)}");
+
+int GetCount(int numTemp)
+{
+    int count = 0;
+    while (numTemp > 0) 
+{
+    numTemp = numTemp / 10;
+    count ++;
+}
+    return count;
+}
