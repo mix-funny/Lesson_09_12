0209ffe22d19f1c0ea0f0dbac3c1cc0bc8a09190
Michael_SVAO <vierumihail7@gmail.com>
2022-12-09 11:36:25 +0300

    Задача Произведение чисел

---
+++ lesson3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел = {GetComposNums(n)}");
+
+int GetComposNums(int number)
+{
+    int compos = 1;
+    while (number > 0)
+    {
+        //Console.Write($"{number} ");
+        compos *= number;
+        number--;
+    }
+    return compos;
+}
